124d6c84c2449e7caabf970dbc9fd52fac612756
xrowkristina <kristina@xrow.de>
2015-05-08 11:21:31 +0200

    tests

--- Repository/ProductRepository.php
+++ Repository/ProductRepository.php
@@ -15,14 +15,6 @@ class ProductRepository
     }
 
     /**
-     * {@inheritdoc}
-     */
-    public function createNew()
-    {
-        return true;
-    }
-
-    /**
      * @param mixed $id
      *
      * @return null|object
@@ -39,6 +31,7 @@ class ProductRepository
     public function findAll()
     {
         // erweitern um die KlassenID, damit nur Produke ausgegeben werden
+        new Criterion\ContentTypeIdentifier(array('product'));
         return $this
             ->getCollectionQueryBuilder()
             ->getQuery()
@@ -63,147 +56,4 @@ class ProductRepository
         ;
     }
 
-    /**
-     * @param array   $criteria
-     * @param array   $orderBy
-     * @param integer $limit
-     * @param integer $offset
-     *
-     * @return array
-     */
-    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
-    {
-        $queryBuilder = $this->getCollectionQueryBuilder();
-
-        $this->applyCriteria($queryBuilder, $criteria);
-        $this->applySorting($queryBuilder, $orderBy);
-
-        if (null !== $limit) {
-            $queryBuilder->setMaxResults($limit);
-        }
-
-        if (null !== $offset) {
-            $queryBuilder->setFirstResult($offset);
-        }
-
-        return $queryBuilder
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function createPaginator(array $criteria = null, array $orderBy = null)
-    {
-        $queryBuilder = $this->getCollectionQueryBuilder();
-
-        $this->applyCriteria($queryBuilder, $criteria);
-        $this->applySorting($queryBuilder, $orderBy);
-
-        return $this->getPaginator($queryBuilder);
-    }
-
-    /**
-     * @param QueryBuilder $queryBuilder
-     *
-     * @return Pagerfanta
-     */
-    public function getPaginator(QueryBuilder $queryBuilder)
-    {
-        return new Pagerfanta(new DoctrineORMAdapter($queryBuilder, true, false));
-    }
-
-    /**
-     * @param array $objects
-     *
-     * @return Pagerfanta
-     */
-    public function getArrayPaginator($objects)
-    {
-        return new Pagerfanta(new ArrayAdapter($objects));
-    }
-
-    /**
-     * @return QueryBuilder
-     */
-    protected function getQueryBuilder()
-    {
-        return $this->createQueryBuilder($this->getAlias());
-    }
-
-    /**
-     * @return QueryBuilder
-     */
-    protected function getCollectionQueryBuilder()
-    {
-        return $this->createQueryBuilder($this->getAlias());
-    }
-
-    /**
-     * @param QueryBuilder $queryBuilder
-     *
-     * @param array $criteria
-     */
-    protected function applyCriteria(QueryBuilder $queryBuilder, array $criteria = null)
-    {
-        if (null === $criteria) {
-            return;
-        }
-
-        foreach ($criteria as $property => $value) {
-            if (null === $value) {
-                $queryBuilder
-                    ->andWhere($queryBuilder->expr()->isNull($this->getPropertyName($property)));
-            } elseif (is_array($value)) {
-                $queryBuilder->andWhere($queryBuilder->expr()->in($this->getPropertyName($property), $value));
-            } elseif ('' !== $value) {
-                $queryBuilder
-                    ->andWhere($queryBuilder->expr()->eq(
-                        $this->getPropertyName($property),
-                        ':' . $key = str_replace('.', '_', $property))
-                    )
-                    ->setParameter($key, $value)
-                ;
-            }
-        }
-    }
-
-    /**
-     * @param QueryBuilder $queryBuilder
-     *
-     * @param array $sorting
-     */
-    protected function applySorting(QueryBuilder $queryBuilder, array $sorting = null)
-    {
-        if (null === $sorting) {
-            return;
-        }
-
-        foreach ($sorting as $property => $order) {
-            if (!empty($order)) {
-                $queryBuilder->addOrderBy($this->getPropertyName($property), $order);
-            }
-        }
-    }
-
-    /**
-     * @param string $name
-     *
-     * @return string
-     */
-    protected function getPropertyName($name)
-    {
-        if (false === strpos($name, '.')) {
-            return $this->getAlias().'.'.$name;
-        }
-
-        return $name;
-    }
-
-    protected function getAlias()
-    {
-        return 'o';
-    }
 }
